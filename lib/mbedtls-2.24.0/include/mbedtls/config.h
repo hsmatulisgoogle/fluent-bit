4f080891c7287f6939a4e203b0e680fe1188ec0a
Henrique S Matulis <69014250+hsmatulisgoogle@users.noreply.github.com>
2020-12-21 15:03:26 -0500

    Update mbed config to allow contexts to be used in multiple threads

--- lib/mbedtls-2.24.0/include/mbedtls/config.h
+++ lib/mbedtls-2.24.0/include/mbedtls/config.h
@@ -1982,7 +1982,7 @@
  *
  * Uncomment this to enable pthread mutexes.
  */
-//#define MBEDTLS_THREADING_PTHREAD
+#define MBEDTLS_THREADING_PTHREAD
 
 /**
  * \def MBEDTLS_USE_PSA_CRYPTO
@@ -3286,7 +3286,7 @@
  *
  * Enable this layer to allow use of mutexes within mbed TLS
  */
-//#define MBEDTLS_THREADING_C
+#define MBEDTLS_THREADING_C
 
 /**
  * \def MBEDTLS_TIMING_C
